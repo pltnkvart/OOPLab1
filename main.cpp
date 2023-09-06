ada7847fda73c55e5a7d850c60816b400ff94893
pltnkvart <artosi@yandex.ru>
2023-09-06 15:13:20 +0300

    fixed memory leaks and cond jump

--- main.cpp
+++ main.cpp
@@ -28,19 +28,23 @@ void push_back(Matrix &matrix, int value, int i, int j) {
     newElem->i = i;
     newElem->j = j;
     newElem->value = value;
+    newElem->next = nullptr;
     if (matrix.head == nullptr) {
         matrix.head = newElem;
-    }
-    if (matrix.tail != nullptr) {
+        matrix.tail = newElem;
+    } else {
         matrix.tail->next = newElem;
+        matrix.tail = newElem;
     }
-    matrix.tail = newElem;
 }
+
 
 Matrix countElements(const Matrix &matrix) {
     Matrix newMatrix;
     newMatrix.columns = matrix.columns;
     newMatrix.lines = matrix.lines;
+    newMatrix.head = nullptr;
+    newMatrix.tail = nullptr;
     int count = 0;
     Element *p = matrix.head;
     while (p) {
@@ -55,37 +59,54 @@ Matrix countElements(const Matrix &matrix) {
         if (count >= 2) {
             push_back(newMatrix, p->value, p->i, p->j);
         }
-        std::cout << std::endl;
         p = p->next;
         count = 0;
     }
     return newMatrix;
 }
 
+
 void print(const Matrix &matrix) {
     for (int i = 0; i < matrix.lines; ++i) {
         for (int j = 0; j < matrix.columns; ++j) {
+            bool printed = false;
             Element *p = matrix.head;
-            while (p) {
+            while (p != nullptr) {
                 if (p->i == i && p->j == j) {
                     std::cout << p->value << " ";
+                    printed = true;
                     break;
                 }
                 p = p->next;
             }
-            if (!p) {
+            if (!printed) {
                 std::cout << "0 ";
             }
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
 }
 
+void erase(Matrix &matrix) {
+    Element *current = matrix.head;
+    while (current) {
+        Element *temp = current;
+        current = current->next;
+        delete temp;
+    }
+    matrix.head = nullptr;
+    matrix.tail = nullptr;
+    matrix.lines = 0;
+    matrix.columns = 0;
+}
 
 Matrix input() {
     Matrix matrix;
     matrix.head = nullptr;
     matrix.tail = nullptr;
+    matrix.lines = 0;
+    matrix.columns = 0;
     try {
         std::cout << "Enter number of lines: --> ";
         matrix.lines = getInt();
@@ -100,16 +121,20 @@ Matrix input() {
         }
     } catch (...) {
         std::cerr << "Error" << std::endl;
+        erase(matrix);
         throw;
     }
     return matrix;
 }
 
 int main() {
-    Matrix matrix;
-    matrix = input();
+    Matrix matrix = input();
+    std::cout << "Sourced matrix:" << std::endl;
     print(matrix);
     Matrix newMatrix = countElements(matrix);
+    std::cout << "New matrix:" << std::endl;
     print(newMatrix);
+    erase(matrix);
+    erase(newMatrix);
     return 0;
 }
