5af4dc494296af004578db728c317502c54f2393
pltnkvart <artosi@yandex.ru>
2023-09-06 18:48:04 +0300

    new files

---
+++ func.h
@@ -0,0 +1,30 @@
+#ifndef LAB1_FUNC_H
+#define LAB1_FUNC_H
+
+#include <iostream>
+
+namespace myMatrix{
+
+    struct Element {
+        int value;
+        int i;
+        int j;
+        Element *next;
+    };
+
+    struct Matrix {
+        int lines;
+        int columns;
+        Element *head;
+        Element *tail;
+    };
+
+    int getInt();
+    Matrix input();
+    Matrix countElements(const Matrix &matrix);
+    void push_back(Matrix &matrix, int value, int i, int j);
+    void print(const Matrix &matrix);
+    void erase(Matrix &matrix);
+}
+
+#endif //LAB1_FUNC_H
